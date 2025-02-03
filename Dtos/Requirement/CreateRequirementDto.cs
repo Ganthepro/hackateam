06d77841aa735c3de51824fe0afbf1bfdbf12435
Pawit Dev <pawito236@gmail.com>
2025-02-03 17:17:09 +0700

    feat: CRUD Requirement

---
+++ Dtos/Requirement/CreateRequirementDto.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace hackateam.Dtos.Requirement
+{
+    public class CreateRequirementDto
+    {
+
+        [Required]
+        public string TeamId { get; set; }
+
+        [Required]
+        public string RoleName { get; set; }
+
+        [Required]
+        [Range(1, 99, ErrorMessage = "MaxSeat must be greater than 0 and not more than 99")]
+        public int MaxSeat { get; set; }
+
+        [Required]
+        public string SkillId { get; set; }
+    }
+}
