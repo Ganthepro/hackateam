d91ef614d099035a550bf81af0c49c2b79a309b1
billza555 <nphisu@gmail.com>
2025-02-17 09:45:11 +0700

    feat: create editproject js

--- wwwroot/js/editProject.js
+++ wwwroot/js/editProject.js
@@ -1,9 +1,43 @@
 const api = "http://localhost:5234";
 
-async function EditProfile() {
-  const title = document.getElementById("fullName")?.value;
-  const description = document.getElementById("header")?.value;
-  const skillId = null;
+async function Submit() {
+  const inputSkill = document.getElementById("skill").value;
+  const datalist = document.getElementById("skills");
+
+  const selectedOption = [...datalist.children].find(
+    (option) => option.value === inputSkill
+  );
+
+  if (selectedOption) {
+    const skillId = selectedOption.dataset.id;
+    EditProject(skillId);
+  } else {
+    const body = { title: inputSkill };
+    try {
+      const response = await fetch(`${api}/Skill`, {
+        method: "Post",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getCookie("token")}`,
+        },
+        body: JSON.stringify(body),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Create Skill failed: ${response.status}`);
+      }
+
+      const data = await response.json();
+      EditProject(data.id);
+    } catch (error) {
+      CreateErrorBlock("Edit Skill failed. Please check your information.");
+    }
+  }
+}
+
+async function EditProject(skillId) {
+  const title = document.getElementById("title").value;
+  const description = document.getElementById("description").value;
 
   try {
     const response = await fetch(`${api}/Project`, {
@@ -22,4 +56,39 @@ async function EditProfile() {
   } catch (error) {
     CreateErrorBlock("Edit Project failed. Please check your information.");
   }
+}
+
+async function SearchSkill() {
+  const skill = document.getElementById("skill").value;
+  if (skill.length < 3) return;
+  const body = { title: skill, limit: 2 };
+  try {
+    const response = await fetch(`${api}/Skill?${body}`, {
+      method: "Get",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getCookie("token")}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Edit Project failed: ${response.status}`);
+    }
+    const data = await response.json();
+    CreateOption(data);
+  } catch (error) {
+    CreateErrorBlock("Enter Wrong Skill");
+  }
+}
+
+function CreateOption(information) {
+  const skills = document.getElementById("skills");
+  skills.innerHTML = "";
+  skills.id = "skills";
+  information.forEach((data) => {
+    const value = document.createElement("option");
+    value.value = data.title;
+    value.dataset.id = data.id;
+    skills.appendChild(value);
+  });
 }
