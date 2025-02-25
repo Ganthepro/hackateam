8b8ce20d8a95e461fd778f03d08514d291adc5d4
billza555 <nphisu@gmail.com>
2025-02-25 13:30:46 +0700

    feat: create Notification page

---
+++ wwwroot/js/notification.js
@@ -0,0 +1,45 @@
+api = "http://localhost:5234";
+
+document.addEventListener("DOMContentLoaded", async function () {
+  await GetNotification(1);
+});
+
+async function GetNotification(userId) {
+  try {
+    const response = await fetch(`${api}/Notification`, {
+      method: "Get",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getCookie("token")}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Get Notification failed: ${response.status}`);
+    }
+
+    const result = await response.json();
+    if (result.length === 0) {
+      CreateNoMessage();
+    } else {
+    }
+    console.log(result);
+  } catch (error) {
+    CreateErrorBlock("Get Notification failed. Please check your data.");
+  }
+}
+
+function CreateNoMessage() {
+  const noNotification = document.createElement("div");
+  noNotification.className = "nomessage";
+  const image = document.createElement("img");
+  image.src =
+    "https://static.vecteezy.com/system/resources/thumbnails/014/814/039/small/a-well-designed-flat-icon-of-no-notification-yet-vector.jpg";
+  image.alt = "No Notification";
+  const message = document.createElement("h4");
+  message.innerText = "No Notification";
+  noNotification.appendChild(image);
+  noNotification.appendChild(message);
+  const messages = document.getElementById("messages");
+  messages.appendChild(noNotification);
+}
