a2b63849ca30af1eeeb70b32253c1aef412cd84d
Pawit Dev <pawito236@gmail.com>
2025-02-26 21:13:46 +0700

    feat: get req / submission by team id

---
+++ Constants/SubmissionMessage.cs
@@ -0,0 +1,9 @@
+namespace hackateam.Constants
+{
+    public static class SubmissionMessage
+    {
+        public const string NOT_FOUND = "Submission not found";
+        public const string ALREADY_EXISTS = "Submission already exists";
+
+    }
+}
