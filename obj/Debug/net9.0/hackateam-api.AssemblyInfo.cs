9d42dd07342c95f374dd9777a6bdb819dbdafbaa
Proxiermax <89377437+Proxiermax@users.noreply.github.com>
2025-02-26 21:34:19 +0700

    style: update colors and add wave grid background to enhance UI

--- obj/Debug/net9.0/hackateam-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/hackateam-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hackateam-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4342849b6b345cd3e8cc6d1f6d32f3f3f70f9dd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c24f514ab39b5e33f5aebb88eb0e598270fc00e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("hackateam-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hackateam-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
