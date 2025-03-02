9b70c58f4e939269cc80d8eadf51558a3f070ae5
Proxiermax <89377437+Proxiermax@users.noreply.github.com>
2025-03-02 15:49:57 +0700

    feat: replace wave grid background with effect grid background and update related scripts

--- obj/Debug/net9.0/hackateam-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/hackateam-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hackateam-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca52077193efc6e2fc92b56d551419d4d5257f16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef0d2cf4891890b9bf0905d3a65f34b63f97e94")]
 [assembly: System.Reflection.AssemblyProductAttribute("hackateam-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hackateam-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
