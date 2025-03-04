de6f6bb4c579b07f8e617d25d82c1aec99ec6294
Proxiermax <89377437+Proxiermax@users.noreply.github.com>
2025-03-04 21:48:44 +0700

    feat: enhance Intro page with new styles and background effects

--- obj/Debug/net9.0/hackateam-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/hackateam-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hackateam-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a692cec0fa902f3167b9d01f378121bb082ca25a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22f3a4073477573695dc5b85c78e8e180e265f1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("hackateam-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hackateam-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
