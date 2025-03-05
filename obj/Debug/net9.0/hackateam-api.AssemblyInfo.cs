2d4505a77f9d148e0eb00f734ac2ad6cbc01e726
Proxiermax <89377437+Proxiermax@users.noreply.github.com>
2025-03-05 15:15:43 +0700

    Implement CRUD functionality for various modules and enhance UI/UX Add CRUD operations for projects, skills, notifications, submissions, and teams. Improve error handling with constants, enhance user interface elements, and implement pagination and filtering across multiple services.

--- obj/Debug/net9.0/hackateam-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/hackateam-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hackateam-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+458eddefbe7b2614caafea17ac08983a31ce2d00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8862d40b245877e9372c0320c0f4f0bd99eb24ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("hackateam-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hackateam-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
