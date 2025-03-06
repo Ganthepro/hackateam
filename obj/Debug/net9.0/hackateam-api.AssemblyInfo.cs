c026850da4d7731a599fc7512d26caebeacce9ab
Proxiermax <89377437+Proxiermax@users.noreply.github.com>
2025-03-06 13:17:07 +0700

    refactor: remove unused file input styles and update assembly info

--- obj/Debug/net9.0/hackateam-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/hackateam-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hackateam-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f799557640c77e255a667c309aeba9f840ebacb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38d0df679df826680c3dc2f49eb660eb28174dd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("hackateam-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hackateam-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
