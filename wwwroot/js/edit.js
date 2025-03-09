8179fb5c874e3eb5e8d5845a9f00104a32b67194
Pawit Dev <pawito236@gmail.com>
2025-03-09 16:42:21 +0700

    fix(on launch team): team.status should be checked as int type not string

--- wwwroot/js/edit.js
+++ wwwroot/js/edit.js
@@ -716,7 +716,7 @@ async function main() {
   updateConfirmButtonState(requirements, submissions);
   if (confirmTeamBtn) {
     confirmTeamBtn.addEventListener("click", async function () {
-      if (team.status !== "Opened") {
+      if (team.status !== 0) {
         alert("Team already confirmed!");
         return;
       }
